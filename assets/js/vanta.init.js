f59bbfde6b6d93a4e447e8f8ed70772bc88fae9f
Tofu016 <cristopherpondoc@gmail.com>
2025-05-10 08:48:50 +0800

    Change background

--- assets/js/vanta.init.js
+++ assets/js/vanta.init.js
@@ -1,12 +1,17 @@
 window.addEventListener('load', () => {
-  VANTA.RINGS({
+  VANTA.CLOUDS({
     el: "#vanta-bg",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
     minHeight: 200.00,
     minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00
+    backgroundColor: 0xff12ff,
+    skyColor: 0xff00d6,
+    cloudColor: 0x4da220,
+    sunColor: 0x969696,
+    sunGlareColor: 0x916c61,
+    sunlightColor: 0xca8446,
+    speed: 2.60
   });
 });
